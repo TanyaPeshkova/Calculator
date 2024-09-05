d1b9c543c82f5aa416586219c2d9944a2adc09ef
TanyaPeshkova <peshkova-2002.01@mail.ru>
2024-09-05 22:16:41 +0700

    функция добавления на дисплей

--- calc.js
+++ calc.js
@@ -2,3 +2,8 @@ function {
 	const display = document.getElementById('input');
 	display.value = '';
 }
+
+function append(value) {
+	const display = document.getElementById('input');
+	display.value += value
+}
