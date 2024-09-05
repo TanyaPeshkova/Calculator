793e5630ee85066eca279e6e675c952bb02ebadd
TanyaPeshkova <peshkova-2002.01@mail.ru>
2024-09-05 22:18:08 +0700

    функция вычисления значения

--- calc.js
+++ calc.js
@@ -7,3 +7,12 @@ function append(value) {
 	const display = document.getElementById('input');
 	display.value += value
 }
+
+function calculate() {
+	const display = document.getElementById('input');
+	try {
+		display.value = eval(display.value);
+	} catch (error) {
+		display.value = 'Ошибка';
+	}
+}
